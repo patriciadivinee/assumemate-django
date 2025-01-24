4652568766aff284b128bd11d46e45a6cd306020
xionxx <patricia.desuuu@gmail.com>
2025-01-24 16:26:19 +0800

    Add .gitignore file

--- base/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-from base import consumers
-
-websocket_urlpatterns = [ 
-    re_path(r'ws/chat/(?P<receiver_id>\d+)/$', consumers.ChatConsumer.as_asgi()), 
-    re_path(r'ws/chat/read/(?P<room_id>\d+)/$', consumers.MessageIsReadConsumer.as_asgi()), 
-    re_path(r'ws/inbox/$', consumers.InboxConsumer.as_asgi()), 
-    ]
